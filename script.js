3e23c4ec7f9b01134936b836750ebc9aad3e2568
FilipeMartins-dev <filipe.martins.dev@gmail.com>
2024-07-09 17:26:25 -0300

    corrigindo nav mobile

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function openMenu(){
     let navMenu = document.querySelector("#aside-nav");
     navMenu.classList.toggle("display-none")
 }
+
+function bodyResize(){
+    if(window.innerWidth >= 768){
+        let navMenu = document.querySelector("#aside-nav")
+        navMenu.classList.add("display-none")
+    }
+}
